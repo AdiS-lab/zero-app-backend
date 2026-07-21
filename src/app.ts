a72d8c4c0581d8ddb9fd7671f3a0b586e9fd002b
Choi Phil <philchoi@icloud.com>
2026-07-21 23:07:31 +0900

    does it work

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,9 @@ import router from './app.router';
 const app = express();
 
 const unused = 'HELLO WORLD';
+const unused1 = 'HELLO WORLD';
+const unused2 = 'HELLO WORLD';
+const unused3 = 'HELLO WORLD';
 
 // middlwares
 app.use(express.json());
