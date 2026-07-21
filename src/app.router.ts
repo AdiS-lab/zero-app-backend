630ebabcdd6ec04a13f7254f7729506ca765454e
Choi Phil <philchoi@icloud.com>
2026-07-21 23:35:54 +0900

    j

--- src/app.router.ts
+++ src/app.router.ts
@@ -1,9 +1,11 @@
-import { Router } from "express";
+import { Router } from 'express';
 
-import apiRoutes from "./routes/api";
+import apiRoutes from './routes/api';
 
 const router = Router();
 
-router.use("/api", apiRoutes);
+router.use('/api', apiRoutes);
+// router.use("/upload", )
+// router.use("/redirect", )
 
 export default router;
